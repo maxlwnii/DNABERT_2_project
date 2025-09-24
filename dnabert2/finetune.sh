ea3392b1c36aa19d6fb0ec7c21cdf5fdf01fcf00
maxlwnii <maxlwndv@gmail.com>
2025-09-24 14:22:02 +0200

    new readme, new train.py, new envronment.yml

---
+++ dnabert2/finetune.sh
@@ -0,0 +1,24 @@
+export TOKENIZERS_PARALLELISM=false
+export DATA_PATH=./data
+export MAX_LENGTH=20
+export LR=3e-5
+
+python train.py \
+    --model_name_or_path zhihan1996/DNABERT-2-117M \
+    --data_path ${DATA_PATH} \
+    --kmer -1 \
+    --run_name DNABERT2_${DATA_PATH} \
+    --model_max_length ${MAX_LENGTH} \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 16 \
+    --gradient_accumulation_steps 1 \
+    --learning_rate ${LR} \
+    --num_train_epochs 5 \
+    --fp16 \
+    --save_steps 200 \
+    --eval_steps 200 \
+    --output_dir output/dnabert2 \
+    --warmup_steps 50 \
+    --logging_steps 100 \
+    --overwrite_output_dir \
+    --log_level info
